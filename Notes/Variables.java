46d06be6217fec4f1906d3b6646c3b6e3b9a7995
Makoto Miyamoto <48812892+MakotoMiyamoto@users.noreply.github.com>
2019-04-26 09:49:23 -0500

    Create Variables.java
    Don't forget that all variables and functions are case-sensitive!

---
+++ Notes/Variables.java
@@ -0,0 +1,30 @@
+public class Main {
+
+  public static void main(String[] args) {
+  
+    System.out.println("Hello, World!");
+    // this function prints the text "Hello World!"
+    // to console. All Strings must be written within
+    // quotation marks.
+    
+    int x1 = (10 + 5) + (2 * 10);
+    // variables can be assigned to mathematical
+    // expressions, such as the one above.
+    
+    int x2 = 12;
+    
+    int x3 = x1 * 2;
+    // variables which have already been defined
+    // can be used in mathematical expressions.
+    
+    int xSum = x1 + x2 + x3;
+    
+    System.out.printn(xSum);
+    
+    // if you don't understand these notes, I suggest you
+    // take an introductory course in programming. Otherwise
+    // you'll only hurt your brain trying to learn.
+  
+  }
+
+}
